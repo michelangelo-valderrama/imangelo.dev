0a135d7789e428df0b9066bf33b11991ebe53fef
Michelangelo Valderrama <imangelo.1984@gmail.com>
2025-01-21 14:57:41 +0100

    remove: -vercel

--- .vercel/output/static/_astro/404.Dbsoe1qz.js
+++
@@ -1,4 +0,0 @@
-const e="404.mdx",n="pages",t="404",o=`import PageHeader from '@/components/view/PageHeader.astro'
-
-<PageHeader title="404" subtitle="Página no encontrada" />
-`,a={},c={type:"content",filePath:"/home/imangelo/Documents/01_Projects/imangelo.dev/src/content/pages/404.mdx",rawData:void 0};export{c as _internal,o as body,n as collection,a as data,e as id,t as slug};
